6eabc5992105fe1559110d6c1b663d9f93d28834
ranupoju0126 <33760139+ranupoju0126@users.noreply.github.com>
2019-03-12 20:52:46 -0400

    Update handler.php

--- myform/handler.php
+++ myform/handler.php
@@ -1,26 +1,11 @@
+
 <?php
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-/*
-Tested working with PHP5.4 and above (including PHP 7 )
+if($_POST["message"]) {
+    mail("your@email.address", "Form to email message", $_POST["message"], "From: an@email.address");
+}
+?>
 
- */
-require_once './vendor/autoload.php';
-
-use FormGuide\Handlx\FormHandler;
-
-
-$pp = new FormHandler(); 
-
-$validator = $pp->getValidator();
-$validator->fields(['name','email'])->areRequired()->maxLength(50);
-$validator->field('email')->isEmail();
-$validator->field('comments')->maxLength(6000);
-
-
-
-
-$pp->sendEmailTo('ravi.shahrukh@gmail.com@gmail.com'); // ← Your email here
-
-echo $pp->process($_POST);
+<form method="post" action="contact.php">
+    <textarea name="message"></textarea>
+    <input type="submit">
+</form>
